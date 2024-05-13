941b839bd2a2272e8fb46d381c43a8726e74123e
mrgb <porrosyhd123@MRGB-P725>
2024-05-13 16:23:37 +0700

    feat: update callback upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,22 @@
 <script>
   import viteLogo from "/vite.svg";
   import Counter from "./lib/Upload.svelte";
+
+  let result = {
+    status: "",
+    data: {},
+  };
+  const onload = (res) => {
+    result = JSON.parse(res);
+  };
+  const processfilerevert = () => {
+    result = {
+      status: "",
+      data: {}
+    }
+  }
+
+  $: result;
 </script>
 
 <main class="flex flex-col items-center justify-center">
@@ -10,10 +26,53 @@
     </a>
   </div>
 
-  <h1>Bisindo Detector</h1>
+  <h1 class="mb-8">Bisindo Detector</h1>
 
-  <div class="card mt-8 w-4/12">
-    <Counter />
+  {#if result.status == "ok"}
+    <div
+      id="alert-border-3"
+      class="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 w-4 h-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <div class="ms-3 text-sm font-medium">
+        Gambar dibawah merupakan huruf {result.data.result} dalam bahasa isyarat
+      </div>
+    </div>
+  {:else if result.status == "error"}
+    <div
+      id="alert-border-2"
+      class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 w-4 h-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <div class="ms-3 text-sm font-medium">
+        {result.message}
+      </div>
+    </div>
+  {/if}
+  <div class="card mt-0 w-4/12">
+    <Counter {onload} {processfilerevert} />
   </div>
 </main>
 
--- src/lib/Upload.svelte
+++ src/lib/Upload.svelte
@@ -2,7 +2,7 @@
   import "filepond/dist/filepond.css";
   import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
-  import FilePond from "./Filepond.svelte"
+  import FilePond from "./Filepond.svelte";
 
   import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
   import FilePondPluginImagePreview from "filepond-plugin-image-preview";
@@ -13,31 +13,51 @@
     FilePondPluginImagePreview
   );
 
+  export let onload;
+  export let processfilerevert;
+
   // a reference to the component, used to call FilePond methods
   // for example `pond.getFiles()` will return the active files
   let pond;
 
   // the name to use for the internal file input
   let name = "file";
+  let apiEndpoint = "http://localhost:5000/predict";
 
-  let apiEndpoint = "http://localhost:5000/predict"
+  let server = {
+    process: {
+      url: apiEndpoint,
+      method: "POST",
+      withCredentials: false,
+      headers: {},
+      timeout: 7000,
+      onload: null,
+      onerror: null,
+      ondata: null,
+    },
+  };
+
+  if (onload) {
+    server.process.onload = onload;
+  }
 
   // handle filepond events
   function handleInit() {
     console.log("FilePond has initialised");
   }
 
-  function handleAddFile(err, fileItem) {
-    console.log("A file has been added", fileItem);
-  }
+  function handleAddFile(err, fileItem) {}
 
+  const handleProcessFile = (err, file) => {};
 </script>
 
 <FilePond
   bind:this={pond}
   {name}
-  server={apiEndpoint}
+  {server}
   allowMultiple={true}
   oninit={handleInit}
   onaddfile={handleAddFile}
+  onprocessfile={handleProcessFile}
+  onprocessfilerevert={processfilerevert}
 />
